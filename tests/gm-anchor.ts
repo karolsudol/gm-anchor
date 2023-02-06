3de46b222f297dcd5e87319dcdcb24ece3f20f6f
elcomendante <karol@finsane.com>
2023-02-06 20:16:09 +0000

    test ok

--- tests/gm-anchor.ts
+++ tests/gm-anchor.ts
@@ -1,16 +1,40 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { GmAnchor } from "../target/types/gm_anchor";
+import { assert, expect } from "chai";
 
 describe("gm-anchor", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  const user = provider.wallet;
   const program = anchor.workspace.GmAnchor as Program<GmAnchor>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Should set name correctly:", async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        user.publicKey,
+        2 * anchor.web3.LAMPORTS_PER_SOL
+      )
+    );
+
+    const gmAccount = anchor.web3.Keypair.generate();
+    const name = "KOKO";
+
+    let tx = await program.rpc.execute(name, {
+      accounts: {
+        gmAccount: gmAccount.publicKey,
+        user: user.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      options: { commitment: "confirmed" },
+      signers: [gmAccount],
+    });
+
+    const storedName = await program.account.greetingAccount.fetch(
+      gmAccount.publicKey
+    );
+
+    expect(storedName.name).equal(name);
+
     console.log("Your transaction signature", tx);
   });
 });
